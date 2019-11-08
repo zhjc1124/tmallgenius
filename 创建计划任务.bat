91fef19c42b29e952dfb1f150e2991533b0deb41
zhjc1124 <zhjc1124@gmail.com>
2019-11-08 23:49:28 +0800

    modify setting

--- 创建计划任务.bat
+++ 创建计划任务.bat
@@ -1,3 +1,6 @@
 echo off
-SCHTASKS /CREATE /SC DAILY /TN tmallgenius /TR %~dp0% %run.vbs /ST 00:01
+set t=00:01
+echo 你将要设置每天启动的时间为%t%
+echo 如果要覆盖任务计划的话，请右键以管理员身份运行
+SCHTASKS /CREATE /SC DAILY /TN tmallgenius /TR %~dp0% %run.vbs /ST %t%
 PAUSE
